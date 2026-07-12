e6890212b6933d991354e7973ae150a23832659c
Peter Steudel <peter.steudel@outlook.com>
2026-07-12 20:22:12 +0200

    Refactor Mileage aggregate to include Date parameter in constructor and factory methods

--- src/backend/Fahrtenbuch.Domain/aggregates/mileage.aggregate.cs
+++ src/backend/Fahrtenbuch.Domain/aggregates/mileage.aggregate.cs
@@ -5,26 +5,26 @@ namespace Fahrtenbuch.Domain.aggregates;
 
 public class Mileage : Aggregate
 {
-
     public MileageId Id { get; init; }
     public CarId CarId { get; init; }
     public decimal Value { get; init; }
+    public DateTime Date { get; init; }
 
-    private Mileage(MileageId id, CarId carId, decimal value)
+    private Mileage(MileageId id, CarId carId, decimal value, DateTime date)
     {
         Id = id;
         CarId = carId;
         Value = value;
+        Date = date;
+    }
+    public static Mileage Create(MileageId id, CarId carId, decimal value, DateTime date)
+    {
+        return new Mileage(id, carId, value, date);
     }
 
-    public static Mileage Create(MileageId id, CarId carId, decimal value)
+    public static Mileage Rehydrate(MileageId id, CarId carId, decimal value, DateTime date)
     {
-        return new Mileage(id, carId, value);
-    }
-
-    public static Mileage Rehydrate(MileageId id, CarId carId, decimal value)
-    {
-        return new Mileage(id, carId, value);
+        return new Mileage(id, carId, value, date);
 
     }
 }
