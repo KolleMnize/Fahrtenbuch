0ac7a4216fdb19dcef1ebe16f6b2d964cc05b0e1
Peter Steudel <peter.steudel@outlook.com>
2026-08-23 15:57:06 +0200

    Add RideManagementController for handling ride operations

---
+++ src/backend/Fahrtenbuch.Api/Controllers/RideManagement.Controller.cs
@@ -0,0 +1,39 @@
+using Fahrtenbuch.Application.Commands;
+using Fahrtenbuch.Application.Querys;
+using Fahrtenbuch.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fahrtenbuch.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RideManagementController(RideManagementService rideManagementService) : ControllerBase
+{
+    [HttpPost(Name = "CreateRide")]
+    public async Task<IActionResult> CreateRide(CreateRideCommand command)
+    {
+        var result = await rideManagementService.Handle(command);
+        if (result.IsError)
+        {
+            return BadRequest(result.Errors);
+        }
+        return Ok();
+    }
+
+    [HttpPut(Name = "EndRide")]
+    public async Task<IActionResult> EndRide(EndRideCommand command)
+    {
+        var result = await rideManagementService.Handle(command);
+        if (result.IsError)
+        {
+            return BadRequest(result.Errors);
+        }
+        return Ok();
+    }
+
+    [HttpGet(Name = "GetRides")]
+    public async Task<GetRidesQueryResult> GetRides([FromQuery] GetRidesQuery query)
+    {
+        return await rideManagementService.Handle(query);
+    }
+}
